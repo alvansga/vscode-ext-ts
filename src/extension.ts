214f986737b83e9b08be4662ddf5b17f0f259b42
ezra <ezra.alvansga@polytron.co.id>
2021-02-17 14:52:17 +0700

    onView activation

--- src/extension.ts
+++ src/extension.ts
@@ -9,13 +9,13 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	vscode.window.registerTreeDataProvider('exampleView', new TreeDataProvider());
+	vscode.window.registerTreeDataProvider('ReferFunction', new TreeDataProvider());
 
 	let disposable = vscode.commands.registerCommand('helloworld.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Testing Tree Data View! Look at side bar by @alvansga');
+		vscode.window.showInformationMessage('Testing Tree Data View! Look at your Explorer\nby @alvansga');
 	});
 	
 	context.subscriptions.push(disposable);
