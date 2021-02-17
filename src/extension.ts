6b53d3da838c8570e345b242974513479bb572b8
ezra <ezra.alvansga@polytron.co.id>
2021-02-17 14:36:46 +0700

    add tree view

--- src/extension.ts
+++ src/extension.ts
@@ -1,36 +1,60 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
+	
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "helloworld" is now active!');
-
+	
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
+	vscode.window.registerTreeDataProvider('exampleView', new TreeDataProvider());
+
 	let disposable = vscode.commands.registerCommand('helloworld.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World! by @alvansga');
-	});
-
-	
-	let disposable2 = vscode.commands.registerCommand('helloworld.start', () => {
-		// The code you place here will be executed every time your command is executed
-		
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Starting...');
+		vscode.window.showInformationMessage('Testing Tree Data View! Look at side bar by @alvansga');
 	});
 	
 	context.subscriptions.push(disposable);
-	context.subscriptions.push(disposable2);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+class TreeDataProvider implements vscode.TreeDataProvider<TreeItem> {
+  onDidChangeTreeData?: vscode.Event<TreeItem|null|undefined>|undefined;
+
+  data: TreeItem[];
+
+  constructor() {
+    this.data = [new TreeItem('cars', [
+      new TreeItem(
+          'Ford', [new TreeItem('Fiesta'), new TreeItem('Focus'), new TreeItem('Mustang')]),
+      new TreeItem(
+          'BMW', [new TreeItem('320'), new TreeItem('X3'), new TreeItem('X5')])
+    ])];
+  }
+
+  getTreeItem(element: TreeItem): vscode.TreeItem|Thenable<vscode.TreeItem> {
+    return element;
+  }
+
+  getChildren(element?: TreeItem|undefined): vscode.ProviderResult<TreeItem[]> {
+    if (element === undefined) {
+      return this.data;
+    }
+    return element.children;
+  }
+}
+
+class TreeItem extends vscode.TreeItem {
+  children: TreeItem[]|undefined;
+
+  constructor(label: string, children?: TreeItem[]) {
+    super(
+        label,
+        children === undefined ? vscode.TreeItemCollapsibleState.None :
+                                 vscode.TreeItemCollapsibleState.Expanded);
+    this.children = children;
+  }
+}
